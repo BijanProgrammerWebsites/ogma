81bdf50086e3f9c19fb4d1591a9ae443451e70eb
BijanProgrammer <bijaneisapour@gmail.com>
2021-11-23 00:55:31 +0330

    [UPDATE] slide - table of contents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,20 @@ import {AppComponent} from './app.component';
 import {IconsModule} from './icons/icons.module';
 import {IllustrationsModule} from './illustrations/illustrations.module';
 import {PagesModule} from './pages/pages.module';
+import {PipesModule} from './pipes/pipes.module';
 import {SlidesModule} from './slides/slides.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, IconsModule, IllustrationsModule, PagesModule, SlidesModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        IconsModule,
+        IllustrationsModule,
+        PagesModule,
+        PipesModule,
+        SlidesModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
--- src/app/slides/slides.module.ts
+++ src/app/slides/slides.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CoverComponent} from './cover/cover.component';
 import {TableOfContentsComponent} from './table-of-contents/table-of-contents.component';
+import {PipesModule} from '../pipes/pipes.module';
 
 @NgModule({
     declarations: [CoverComponent, TableOfContentsComponent],
     exports: [CoverComponent, TableOfContentsComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, PipesModule],
 })
 export class SlidesModule {}
