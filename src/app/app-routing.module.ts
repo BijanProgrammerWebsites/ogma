839ff01ed5d25c95b4421246ea6461413b3b0edc
BijanProgrammer <bijaneisapour@gmail.com>
2021-11-23 10:47:34 +0330

    [CI-CD][INIT]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {GalleryComponent} from './pages/gallery/gallery.component';
 import {SlideshowComponent} from './pages/slideshow/slideshow.component';
 
 const routes: Routes = [
-    {path: '', component: GalleryComponent},
+    {path: '', pathMatch: 'full', redirectTo: 'slideshow/1'},
     {path: 'slideshow', redirectTo: 'slideshow/1'},
     {path: 'slideshow/:index', component: SlideshowComponent},
 ];
